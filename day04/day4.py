ae38da2e795eaa4bd42cc6192f2a97ff540daa15
MrNarman <godricknarman21@gmail.com>
2025-06-15 12:14:37 +0300

    Day 04: Practice on randomisation and module creation

---
+++ day04/day4.py
@@ -0,0 +1,14 @@
+import random
+# import my_module
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+# print(my_module.my_unique_number)
+# my_module.module_helo()
+
+random_number = random.random() * 10
+print(random_number)
+
+random_float = random.uniform(0, 10)
+print(random_float)
---
+++ day04/my_module.py
@@ -0,0 +1,6 @@
+# Practice on creating and using modules
+
+my_unique_number = 3.142
+
+def module_helo():
+    print("Hello. This is my module")
