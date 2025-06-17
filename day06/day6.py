eb92bc9d3076bb4537e9617884610e4115008b13
MrNarman <godricknarman21@gmail.com>
2025-06-17 23:28:40 +0300

    Day 06: While loops

---
+++ day06/day6.py
@@ -0,0 +1,30 @@
+# WHILE LOOPS
+# Code was mainly on the online editor
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def circular_move():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if wall_in_front():
+#         circular_move()
+#     else:
+#         move()
+
+'''
+    Code made a robot walk through a maze
+'''
