3d4137860f27dbf77be6524c0b75647f77bd3c29
MrNarman <godricknarman21@gmail.com>
2025-06-22 22:18:40 +0300

    Shortened the code by simplifying the casesar function to accommodate both encode and decode without the respective functions

--- day08/Caesar-Cipher.py
+++ day08/Caesar-Cipher.py
@@ -5,34 +5,15 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input('Type your message:\n').lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text, shift_amount):
+def caesar(original_text, shift_amount, direction_to_take):
     cipher_text = ""
     for char in original_text:
+
+        if direction_to_take == 'decode':
+            shift_amount *= -1
         shifted_position = alphabet.index(char) + shift_amount
         shifted_position %= len(alphabet)
         cipher_text += alphabet[shifted_position]
 
-    print(f"Here is your encoded result: {cipher_text}")
-# TODO-1: Create a function called decrypt() that takes original_text and shift_amount as 2 inputs
-# TODO-2: Inside the decrypt() function, shift each letter of the original_text backwards in the alphabet backwards by
-#  the shift_amount and print the decrypted text
-def decrypt(original_text, shift_amount):
-    plain_text = ""
-    for char in original_text:
-        shifted_position = alphabet.index(char) - shift_amount
-        shifted_position %= len(alphabet)
-        plain_text += alphabet[shifted_position]
-
-    print(f"Here is your encoded result: {plain_text}")
-
-# TODO-3: Combine the 'encrypt' and 'decrypt' functions into a single function called caesar().
-#  Use the value of the user chosen direction variable to determine which functionality to use. Call the Caesar
-#  function instead of encrypt/decrypt and pass in all the three variables direction/text/shift
-def caesar(direction_to_take):
-    if direction_to_take == 'encode':
-        encrypt(original_text=text, shift_amount=shift)
-    elif direction_to_take == 'decode':
-        decrypt(original_text=text, shift_amount=shift)
-    else:
-        print("Invalid choice!")
-caesar(direction)
+    print(f"Here is your {direction_to_take}d result: {cipher_text}")
+caesar(original_text=text, shift_amount=shift, direction_to_take=direction)
