06f3098197795829b35081ac2dbba761bc83057a
MrNarman <godricknarman21@gmail.com>
2025-06-22 22:57:02 +0300

    Day08: Functions and Caesar Cipher(Organised the code)

--- day08/Caesar-Cipher.py
+++ day08/Caesar-Cipher.py
@@ -3,19 +3,34 @@ from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 print(logo)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input('Type your message:\n').lower()
-shift = int(input("Type the shift number:\n"))
 
 def caesar(original_text, shift_amount, direction_to_take):
     cipher_text = ""
-    for char in original_text:
+    if direction_to_take == 'decode':
+        shift_amount *= -1
 
-        if direction_to_take == 'decode':
-            shift_amount *= -1
-        shifted_position = alphabet.index(char) + shift_amount
-        shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
+    for char in original_text:
+        if char not in alphabet:
+            cipher_text += char
+        else:
+            shifted_position = alphabet.index(char) + shift_amount
+            shifted_position %= len(alphabet)
+            cipher_text += alphabet[shifted_position]
 
     print(f"Here is your {direction_to_take}d result: {cipher_text}")
-caesar(original_text=text, shift_amount=shift, direction_to_take=direction)
+
+# TODO-3: figure out to restart the program
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input('Type your message:\n').lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, direction_to_take=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n").lower()
+    if restart == 'no':
+        should_continue = False
+        print("Good Bye")
+
