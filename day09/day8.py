010133ae86551b770f7ee2396ae04a8197dd3d33
MrNarman <godricknarman21@gmail.com>
2025-06-23 22:12:45 +0300

    Day 09: Dictionaries

---
+++ day09/day8.py
@@ -0,0 +1,21 @@
+programming_dictionary = {
+    "bug":"An error in a program that causes it to run unexpectedly",
+    "function":"a piece of code to call over and over again"
+
+}
+
+# print(programming_dictionary["bug"])
+#
+# programming_dictionary["loop"] = "action of coding over and over "
+# print(programming_dictionary)
+# #
+# empty_dictionary = {}
+# programming_dictionary = {}
+# print(programming_dictionary)
+#
+# programming_dictionary["bug"] = "godrick narman"
+# print(programming_dictionary)
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
