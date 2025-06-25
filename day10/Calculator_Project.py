b3d9f2e963cea812d8e74c1309d84fc3c2cfbb0e
MrNarman <godricknarman21@gmail.com>
2025-06-25 23:25:04 +0300

    Day10:CALCULATOR PROJECT(Functions with outputs)

--- day10/Calculator_Project.py
+++ day10/Calculator_Project.py
@@ -1,3 +1,43 @@
 from Calculator_Logo import logo1, logo2
+def add (n1, n2):
+    return n1 + n2
 
-print(logo1 + logo2)
+def subtract (n1, n2):
+    return n1 - n2
+
+def multiply (n1, n2):
+    return n1 * n2
+
+def divide (n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+def calculator():
+    print(logo1 + logo2)
+    should_accumulate = True
+    num1 = float(input("What is the first number: "))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+
+        num2 = float(input("What is the next number: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer} or \ntype 'n' to cancel calculation: ")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+calculator()
