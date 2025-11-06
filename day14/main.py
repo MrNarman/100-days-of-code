e4d0b063b299bf382b40cdffdd6ffcf8976e08ef
MrNarman <godricknarman21@gmail.com>
2025-11-06 19:10:45 +0300

    Game stops when the user fails in any attempt. User score compunds with each success

--- day14/main.py
+++ day14/main.py
@@ -3,18 +3,20 @@ from art import vs
 from game_data import data
 from random import randint
 
+user_score = 0
+# detail_index_A = randint(0, 49)
+# detail_index_B = randint(0, 49)
+detail_index_A = 0
+detail_index_B = 1
+
+game_continue = True
 
 # todo: print art and the comparison statements | Include also the score
-
-user_score = 0
-detail_index_A = randint(0, 49)
-detail_index_B = randint(0, 49)
 def main_game_structure(score):
     print(f"{logo}\nyour score is: {score}")
     get_data_A(detail_index_A)
     print(vs)
     get_data_B(detail_index_B)
-
 
 # todo: get data from game_data file to form the questions
 def get_data_A(index):
@@ -28,13 +30,32 @@ def get_data_B(index):
     """
     print(f"Against B: {data[index]['name']},a {data[index]['description']}, from {data[index]['country']} ")
 
-# Call the function of the game
-main_game_structure(user_score)
+# Function to get follower count from the provided index
+def get_followers(index):
+    follower_count = data[index]['follower_count']
+    return follower_count
 
-# todo: get comparisons for statements A and B
-user_input = input("Who has more followers? Type 'A' or 'B': ").lower()
+while game_continue:
+    # Call the function of the game
+    main_game_structure(user_score)
+
+    # todo: get comparisons for statements A and B
+    user_guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    def check_answer(user_input, followers_A, followers_B):
+        if followers_A > followers_B:
+            return user_input == 'a'
+        else:
+            return user_input == 'b'
+
+    # todo: stop the game when the user fails and print out score
+    if check_answer(user_guess, get_followers(detail_index_A), get_followers(detail_index_B)) == False:
+        print(f"That's wrong. Your final score is {user_score}")
+        break
+    else:
+        user_score += 1
 
 # todo: Switch statement B to A incase user is correct
-# todo: stop the game when the user fails and print out score
+
 
 
