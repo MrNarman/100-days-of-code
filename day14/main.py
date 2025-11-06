8a67e639ca9155d8ab0ea838755e49a8a5ee503e
MrNarman <godricknarman21@gmail.com>
2025-11-06 19:57:20 +0300

    Questions switch up when user passes the question

--- day14/main.py
+++ day14/main.py
@@ -4,11 +4,8 @@ from game_data import data
 from random import randint
 
 user_score = 0
-# detail_index_A = randint(0, 49)
-# detail_index_B = randint(0, 49)
-detail_index_A = 0
-detail_index_B = 1
-
+detail_index_A = randint(0, 49)
+detail_index_B = randint(0, 49)
 game_continue = True
 
 # todo: print art and the comparison statements | Include also the score
@@ -35,6 +32,7 @@ def get_followers(index):
     follower_count = data[index]['follower_count']
     return follower_count
 
+# main code
 while game_continue:
     # Call the function of the game
     main_game_structure(user_score)
@@ -49,13 +47,16 @@ while game_continue:
             return user_input == 'b'
 
     # todo: stop the game when the user fails and print out score
-    if check_answer(user_guess, get_followers(detail_index_A), get_followers(detail_index_B)) == False:
+    if not check_answer(user_guess, get_followers(detail_index_A), get_followers(detail_index_B)):
+        # Clear the screen after failure and print final score
+        print("\n"*50 + logo)
         print(f"That's wrong. Your final score is {user_score}")
         break
     else:
+        detail_index_A = detail_index_B
+        if detail_index_A == detail_index_B:
+            detail_index_B = randint(0, 49)
+
         user_score += 1
-
-# todo: Switch statement B to A incase user is correct
-
-
-
+        print("\n" * 50)
+        
