00f1f9b5ae7606f739099ca243e3d86fabd4ea63
MrNarman <godricknarman21@gmail.com>
2025-11-08 13:27:31 +0300

    Added functions to process latte and cappuccino orders

--- day15/main.py
+++ day15/main.py
@@ -54,11 +54,48 @@ def make_espresso(resources_water, resources_coffee):
     if resources_water < espresso_water:
         print("Not enough water to make an espresso!")
     elif resources_coffee < espresso_coffee:
-        print("Not enough coffee to make and espresso!")
+        print("Not enough coffee to make an espresso!")
     else:
         resources['water'] = resources_water - espresso_water
         resources['coffee'] = resources_coffee - espresso_coffee
         print("Here is your espresso ☕️. Enjoy!")
+
+def make_latte(resources_water, resources_milk, resources_coffee):
+    """Calculates the resources to be deducted when making a latte"""
+    latte_water = MENU['latte']['ingredients']['water']
+    latte_milk = MENU['latte']['ingredients']['milk']
+    latte_coffee = MENU['latte']['ingredients']['coffee']
+
+    if resources_water < latte_water:
+        print("Not enough water to make an latte!")
+    elif resources_milk < latte_milk:
+        print("Not enough milk to make a latte!")
+    elif resources_coffee < latte_coffee:
+        print("Not enough coffee to make a latte!")
+    else:
+        resources['water'] = resources_water - latte_water
+        resources['milk'] = resources_milk - latte_milk
+        resources['coffee'] = resources_coffee - latte_coffee
+        print("Here is your latte ☕️. Enjoy!")
+
+
+def make_cappuccino(resources_water, resources_milk, resources_coffee):
+    """Calculates the resources to be deducted when making a cappuccino"""
+    cappuccino_water = MENU['cappuccino']['ingredients']['water']
+    cappuccino_milk = MENU['cappuccino']['ingredients']['milk']
+    cappuccino_coffee = MENU['cappuccino']['ingredients']['coffee']
+
+    if resources_water < cappuccino_water:
+        print("Not enough water to make a cappuccino!")
+    elif resources_milk < cappuccino_milk:
+        print("Not enough milk to make a cappuccino!")
+    elif resources_coffee < cappuccino_coffee:
+        print("Not enough coffee to make a cappuccino!")
+    else:
+        resources['water'] = resources_water - cappuccino_water
+        resources['milk'] = resources_milk - cappuccino_milk
+        resources['coffee'] = resources_coffee - cappuccino_coffee
+        print("Here is your cappuccino ☕️. Enjoy!")
 
 
 make_coffee = True
@@ -66,7 +103,7 @@ while make_coffee:
     # todo: Prompt user by asking “What would you like? (espresso/latte/cappuccino):"
     user_coffee_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
     # TEST ENVIRONMENT- TO BE DELETED
-    make_espresso(resources['water'], resources['coffee'])
+    make_cappuccino(resources['water'], resources['milk'], resources['coffee'])
     # END OF TEST ENVIRONMENT
 
     # print("Please insert coins.")
