3cc6d4c237e8c9544f24699c216251a137dbe610
MrNarman <godricknarman21@gmail.com>
2026-05-20 16:40:55 +0300

    Day 18 module installation and turtle pracice

---
+++ day18/main.py
@@ -0,0 +1,30 @@
+from turtle import Turtle, Screen
+
+# tim = Turtle()
+# tim.shape("turtle")
+# tim.color("brown")
+# tim.pencolor("blue")
+
+# # for t in range(4):
+# #     tim.forward(100)
+# #     tim.right(90)
+
+# for _ in range (10):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+
+import heroes
+name = heroes.gen()
+print(name)
+
+
+
+
+
+
+
+
+# screen = Screen()
+# screen.exitonclick()
