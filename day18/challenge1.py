72a6a2c3da12f1464a5fec684a9d28df9d2db17f
MrNarman <godricknarman21@gmail.com>
2026-05-20 17:55:48 +0300

    Day18 challenge 1 solution

---
+++ day18/challenge1.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+tim = Turtle()
+tim.shape("turtle")
+
+i = 0
+sides = [3, 4, 5, 6, 7, 8, 9, 10]
+colors = ["red", "blue", "green", "yellow", "orange", "purple", "black", "cyan"]
+
+for _ in range(len(sides)):
+    angle_to_turn = 360/(sides[i])
+    tim.pencolor(colors[i])
+    for _ in range (sides[i]):
+        tim.right(angle_to_turn)
+        tim.forward(100)
+    i += 1
+
+screen = Screen()
+screen.exitonclick()
