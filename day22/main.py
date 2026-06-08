2635861505749a0435aa13c1da0546a718da91bb
MrNarman <godricknarman21@gmail.com>
2026-06-08 16:36:53 +0300

    Added ball to the screen

--- day22/main.py
+++ day22/main.py
@@ -1,5 +1,9 @@
-from turtle import Turtle, Screen
+import time
+
+from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,15 +13,18 @@ screen.tracer(0)
 
 right_paddle = Paddle(350, 0)
 left_paddle = Paddle(-350, 0)
+ball = Ball()
 
-
-
-# screen.listen()
-# screen.onkey(go_up, "Up")
-# screen.onkey(go_down, "Down")
+screen.listen()
+screen.onkey(right_paddle.go_up, "Up")
+screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
