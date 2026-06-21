ca514516ba2427f9aeb89f3f2c46e22caba022b4
MrNarman <godricknarman21@gmail.com>
2026-06-21 11:57:12 +0300

    Added turtle, vehicles and implemented move functionality for both

--- day23/main.py
+++ day23/main.py
@@ -8,7 +8,17 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car = CarManager()
+
+screen.listen()
+screen.onkey(player.move_turtle, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car.create_car()
+    car.move_cars()
+    
